8f078ce5f48190cbe45284f8a103e2fd59e07126
erose357 <erose357@gmail.com>
2017-09-02 17:37:09 -0600

    Remove validations for uid to allow for manual login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
-  validates :uid, presence: true
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: true
