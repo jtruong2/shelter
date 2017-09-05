309f1c913b59998b9878900e43b3e8070a858b7e
wratterman <will.ratterman@gmail.com>
2017-09-05 15:15:14 -0600

    got all tests passing with VCR

--- spec/features/host_can_see_a_list_of_their_host_shelters_spec.rb
+++ spec/features/host_can_see_a_list_of_their_host_shelters_spec.rb
@@ -9,35 +9,37 @@ RSpec.describe "Host Shelter Owner Can" do
   end
 
   it "Sees a list of their properties at the Properties Index Page" do
+    VCR.use_cassette("features/user_sees_more_properties") do
 
-    @user.properties.create(:street_address => "1701 Bryant St",
-                            :city => "Denver",
-                            :state => "Colorado",
-                            :rooms_available => 2)
+      @user.properties.create(:street_address => "1701 Bryant St",
+                              :city => "Denver",
+                              :state => "Colorado",
+                              :rooms_available => 2)
 
-    @user.properties.create(:street_address => "1331 17th St",
-                            :city => "Denver",
-                            :state => "Colorado",
-                            :rooms_available => 12)
+      @user.properties.create(:street_address => "1331 17th St",
+                              :city => "Denver",
+                              :state => "Colorado",
+                              :rooms_available => 12)
 
-    @user.owner!
+      @user.owner!
 
-   allow_any_instance_of(ApplicationController).to receive(:current_user).
-   and_return(@user)
+     allow_any_instance_of(ApplicationController).to receive(:current_user).
+     and_return(@user)
 
-   visit '/host_shelters/properties'
+     visit '/host_shelters/properties'
 
-   expect(@user.owner?).to eq true
-   expect(current_path).to eq host_shelters_properties_path
+     expect(@user.owner?).to eq true
+     expect(current_path).to eq host_shelters_properties_path
 
-   expect(page).to have_content @user.properties.first.street_address
-   expect(page).to have_content @user.properties.first.city
-   expect(page).to have_content @user.properties.first.state
-   expect(page).to have_content @user.properties.first.rooms_available
+     expect(page).to have_content @user.properties.first.street_address
+     expect(page).to have_content @user.properties.first.city
+     expect(page).to have_content @user.properties.first.state
+     expect(page).to have_content @user.properties.first.rooms_available
 
-   expect(page).to have_content @user.properties.second.street_address
-   expect(page).to have_content @user.properties.second.city
-   expect(page).to have_content @user.properties.second.state
-   expect(page).to have_content @user.properties.second.rooms_available
+     expect(page).to have_content @user.properties.second.street_address
+     expect(page).to have_content @user.properties.second.city
+     expect(page).to have_content @user.properties.second.state
+     expect(page).to have_content @user.properties.second.rooms_available
+   end
   end
 end
--- spec/features/host_can_see_individual_page_of_their_shelter_spec.rb
+++ spec/features/host_can_see_individual_page_of_their_shelter_spec.rb
@@ -9,24 +9,25 @@ RSpec.describe "Host Shelter Owner Can" do
   end
 
   it "Sees a list of their properties at the Properties Show Page" do
+    VCR.use_cassette("features/user_sees_their_properties") do
+      property = @user.properties.create!(:street_address => "1701 Bryant St",
+                              :city => "Denver",
+                              :state => "CO",
+                              :rooms_available => 2)
 
-    property = @user.properties.create!(:street_address => "1701 Bryant St",
-                            :city => "Denver",
-                            :state => "CO",
-                            :rooms_available => 2)
+      @user.owner!
 
-    @user.owner!
+      allow_any_instance_of(ApplicationController).to receive(:current_user).
+      and_return(@user)
 
-   allow_any_instance_of(ApplicationController).to receive(:current_user).
-   and_return(@user)
+      visit "/host_shelters/#{property.id}"
 
-   visit "/host_shelters/#{property.id}"
-
-   expect(@user.owner?).to eq true
-   expect(current_path).to eq host_shelters_path(property.id)
-   expect(page).to have_content @user.properties.first.street_address
-   expect(page).to have_content @user.properties.first.city
-   expect(page).to have_content @user.properties.first.state
-   expect(page).to have_content @user.properties.first.rooms_available
+      expect(@user.owner?).to eq true
+      expect(current_path).to eq host_shelters_path(property.id)
+      expect(page).to have_content @user.properties.first.street_address
+      expect(page).to have_content @user.properties.first.city
+      expect(page).to have_content @user.properties.first.state
+      expect(page).to have_content @user.properties.first.rooms_available
+   end
   end
 end
