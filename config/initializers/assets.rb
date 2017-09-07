0c5c0ba64305de72e1604074233984a239ce93ed
Mark Van Akkeren <MarkVanAkkeren@gmail.com>
2017-09-07 12:00:15 -0600

    navigation bar html/css/js added unused partials deleted stylesheets and js added partials to layouts/application added

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,7 +7,7 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
-Rails.application.config.assets.precompile += %w( main.css index.css jquery.min.js main.js skel.min.js util.js )
+Rails.application.config.assets.precompile += %w( main.css index.css menu.js jquery.min.js main.js skel.min.js util.js )
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
