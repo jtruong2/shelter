b28630ba456c76798d24e211c14b0035ebd858ec
Wil Donovan <wil.donovan@gmail.com>
2017-09-07 15:05:14 -0600

    seed data finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,4 +11,38 @@ def seed_roles
   Role.create(name: "platform_admin")
 end
 
+def seed_users
+  counter = 0
+  100.times do
+    User.create(first_name: Faker::Name.first_name,
+              last_name: Faker::Name.last_name,
+              email: Faker::Internet.email,
+              password: "password")
+    counter += 1
+    puts "User ##{counter} created"
+  end
+end
+
+def user_owners
+  @users = User.where(id: 0..10)
+end
+
+def seed_owners
+  @users.each {|user| user.owner!}
+end
+
+def seed_properties
+  1000.times do
+    property = @users.sample.properties.create(:street_address => Faker::Address.street_address,
+                                   :city => Faker::Address.city,
+                                   :state =>Faker::Address.state,
+                                   :rooms_available => Faker::Number.between(1, 10)
+                                  )
+    puts "Property #{property.street_address} created"
+  end
+end
 seed_roles
+seed_users
+user_owners
+seed_owners
+seed_properties
