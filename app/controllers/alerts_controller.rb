aa06ee0c78bcd2bfb71765e6924cbbf2889f4f28
Mark Van Akkeren <MarkVanAkkeren@gmail.com>
2017-09-07 16:12:16 -0600

    making things pretty

--- app/controllers/alerts_controller.rb
+++ app/controllers/alerts_controller.rb
@@ -2,5 +2,6 @@ class AlertsController < ApplicationController
   def index
     state = params[:state]
     state ? @alerts = Alert.find_by(state) : @alerts = Alert.find_all
+    @st = params[:state]
   end
 end
