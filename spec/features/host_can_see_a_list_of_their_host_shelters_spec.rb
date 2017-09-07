98cea4de53877dbb23071a70a6396edab4527927
Wil Donovan <wil.donovan@gmail.com>
2017-09-07 16:50:49 -0600

    tests passing master now

--- spec/features/host_can_see_a_list_of_their_host_shelters_spec.rb
+++ spec/features/host_can_see_a_list_of_their_host_shelters_spec.rb
@@ -7,7 +7,6 @@ RSpec.describe "Host Shelter Owner Can" do
     @user = create(:user)
   end
 
-<<<<<<< HEAD
   scenario "sends default request" do
     VCR.use_cassette("properties/view_index") do
 
@@ -22,29 +21,11 @@ RSpec.describe "Host Shelter Owner Can" do
 
      visit '/host_shelters/properties'
 
-=======
-  it "Sees a list of their properties at the Properties Index Page" do
-    VCR.use_cassette("features/see_props") do
-      @user.properties.create(:street_address => "1331 17th St",
-                              :city => "Denver",
-                              :state => "Colorado",
-                              :rooms_available => 2)
-
-      @user.owner!
-
-     allow_any_instance_of(ApplicationController).to receive(:current_user).
-     and_return(@user)
-
-     visit '/host_shelters/properties'
-
->>>>>>> master
      expect(@user.owner?).to eq true
      expect(current_path).to eq host_shelters_properties_path
      expect(page).to have_content @user.properties.first.street_address
      expect(page).to have_content @user.properties.first.city
      expect(page).to have_content @user.properties.first.state
-<<<<<<< HEAD
-     expect(page).to have_content @user.properties.first.rooms_available
     end
   end
 
@@ -60,23 +41,6 @@ RSpec.describe "Host Shelter Owner Can" do
                                     :city => "Denver",
                                     :state => "Colorado",
                                     :rooms_available => 12)
-=======
-   end
-  end
-
-  it "Sees a list of their properties at the Properties Index Page" do
-    VCR.use_cassette("features/see_more_props") do
-
-      @user.properties.create(:street_address => "1701 Bryant St",
-                              :city => "Denver",
-                              :state => "Colorado",
-                              :rooms_available => 2)
-
-      @user.properties.create(:street_address => "1331 17th St",
-                              :city => "Denver",
-                              :state => "Colorado",
-                              :rooms_available => 12)
->>>>>>> master
 
       @user.owner!
 
@@ -91,18 +55,10 @@ RSpec.describe "Host Shelter Owner Can" do
      expect(page).to have_content @user.properties.first.street_address
      expect(page).to have_content @user.properties.first.city
      expect(page).to have_content @user.properties.first.state
-<<<<<<< HEAD
-     expect(page).to have_content @user.properties.first.rooms_available
-=======
->>>>>>> master
 
      expect(page).to have_content @user.properties.second.street_address
      expect(page).to have_content @user.properties.second.city
      expect(page).to have_content @user.properties.second.state
-<<<<<<< HEAD
-     expect(page).to have_content @user.properties.second.rooms_available
-=======
->>>>>>> master
     end
   end
 end
